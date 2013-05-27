db940e6d3974881994472a77e23bc92917928305
Kito Cheng <kito@0xlab.org>
2013-05-27 22:37:12 +0800

    Add Makefile

--- test.sh
+++ test.sh
@@ -3,4 +3,8 @@ chmod a+x ./repo
 
 ./repo init -u https://github.com/olibc/manifest.git
 ./repo sync
+echo "OLIBC_CONF:=.config-olibc" > Makefile
+echo "-include $$(OLIBC_CONF)" >> Makefile
+echo "include build/core/olibc.mk" >> Makefile
+echo "include build/core/main.mk" >> Makefile
 ./BUILD.sh -j10
